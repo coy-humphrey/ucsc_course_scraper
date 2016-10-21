28a328e4fea002046fb73adabaf5a62e534350c8
Coy Humphrey <coy@coyhumphrey.com>
2016-10-21 14:01:26 -0700

    Added comments to clarify some logic

--- web_parse.py
+++ web_parse.py
@@ -11,9 +11,19 @@ def parse_classes( html_contents ):
 def courses( soup ):
     return soup.find_all('td', class_="course-name")
 
+# Given a course, the first a element will be a link to the course
+# web page, with the link title being the full name of the course
+# Format is: COURSENUMBER: Full Course Name
 def course_name( course ):
     return course.find_next('a').contents[0]
 
+# Given a course, a 'tr' will contain 4 'td's, each 'td' representing a quarter.
+# If a 'td's class name is just 'class', then it will contain valid classes.
+# Otherwise, if it is 'class inactive', it will have no classes.
+# Grabbing the class name gives us either ['class'] or ['class', 'invalid'],
+# so we can just check to see if the length is 1 to know if it's valid.
+# The contents of each 'a' within a 'td' will be "Section X".
+# After each 'a', there will be a 'br' followed by the professor's name.
 def professors( course ):
     result = {}
     quarters = ['fall', 'winter', 'spring', 'summer']
@@ -22,12 +32,8 @@ def professors( course ):
                             for a in listing.find_all('a')
                             if listing.get('class') and len(listing.get('class') ) == 1 }
     return result
-    # return { a.contents[0]:  a.next_sibling.next_sibling.strip() 
-    #          for td in course.find_next('tr').find_all('td')
-    #          for a in td.find_all('a')
-    #          if td.get('class') and len( td.get('class') ) == 1 }
 
-# For now we just download a single course page,
+# For now we just manually download a single course page,
 # rename it to ucsc.html, and test with it
 # Future plan is to take a list of filenames in
 # argv, parse each file and dump into same csv
