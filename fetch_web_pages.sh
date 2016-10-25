5fb52564213c23305fd035a810ca673020d03e50
Coy Humphrey <coy@coyhumphrey.com>
2016-10-25 10:27:27 -0700

    Fetch web pages now runs web_parse on fetched pages

--- fetch_web_pages.sh
+++ fetch_web_pages.sh
@@ -3,6 +3,8 @@
 TARGET_DIR="./web_pages"
 SUBJECTS=( "cmps" "cmpe" "ee" "ams" "cmpm" )
 PAGE_URL="https://courses.soe.ucsc.edu/courses/"
+WEB_PARSER="python web_parse.py"
+RESULTS_FILE="courses.json"
 
 mkdir -p ${TARGET_DIR}
 
@@ -11,6 +13,12 @@ do
     echo $year
     for subject in ${SUBJECTS[*]};
     do
-	curl  "${PAGE_URL}${subject}?year=${year}"  > "${TARGET_DIR}/${subject}_${year}.html"
+	FILE_NAME="${TARGET_DIR}/${subject}_${year}.html"
+	if [ ! -f ${FILE_NAME} ]
+	then
+	    curl  "${PAGE_URL}${subject}?year=${year}"  > "${FILE_NAME}"
+	fi
     done
 done
+
+${WEB_PARSER} ${TARGET_DIR}/* > ${RESULTS_FILE}
