948debdd3ba416dc6e75e13a2e8ec96317909c57
Jayden Navarro <jdndeveloper@gmail.com>
2016-10-25 10:53:38 -0700

    Also prints to stdout now

--- RunTranscript.sh
+++ RunTranscript.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -jar tabula-0.9.1-jar-with-dependencies.jar -p all $1 | python generate_transcript.py > $1.txt
+java -jar tabula-0.9.1-jar-with-dependencies.jar -p all $1 | python generate_transcript.py | tee $1.txt
